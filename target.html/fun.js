51d4221d6eb58a83861c6062db269879fb1de39b
Mukesh Utmani <109408900+mukeshkumarutmani@users.noreply.github.com>
2023-09-26 14:32:46 +0500

    Update fun.js

--- target.html/fun.js
+++ target.html/fun.js
@@ -1,12 +1,26 @@
-function startProgress() {
-    const progressBar = document.getElementById("progress");
-    let width = 0;
-    const interval = setInterval(() => {
-        if (width >= 100) {
-            clearInterval(interval);
-        } else {
-            width++;
-            progressBar.style.width = width + "%";
-        }
-    }, 10);
-}
+// question no 50
+document.addEventListener("DOMContentLoaded", () => {
+    const slides = document.querySelectorAll(".carousel-slide");
+    const prevButton = document.querySelector(".carousel-prev");
+    const nextButton = document.querySelector(".carousel-next");
+    let currentIndex = 0;
+
+    function showSlide(index) {
+        slides.forEach((slide, i) => {
+            slide.style.transform = `translateX(${100 * (i - index)}%)`;
+        });
+    }
+
+    prevButton.addEventListener("click", () => {
+        currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+        showSlide(currentIndex);
+    });
+
+    nextButton.addEventListener("click", () => {
+        currentIndex = (currentIndex + 1) % slides.length;
+        showSlide(currentIndex);
+    });
+
+    // Initially show the first slide
+    showSlide(currentIndex);
+});
